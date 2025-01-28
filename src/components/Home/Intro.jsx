0f06b95293533f462303742f1fdb7941f9f545f8
jim <jimcnerf@gmail.com>
2025-01-27 19:27:54 -0800

    Tried to add typing animation
    right now trying to implement typing animation

---
+++ src/components/Home/Intro.jsx
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import './Intro.css';
+// import Typed from 'react-typed';
+
+// const NavBar = () => {
+//   return (
+//     <div className='Intro'>
+//         <h1 className ="title">Hi, my name is Jim</h1>
+//         <div className='Animated-Typing'>
+//             <Typed
+//                 string={[
+//                     "I am a Student",
+//                     "I am a Software Engineer",
+//                     "I am a developer",
+//                 ]}
+//                 typeSpeed={100}
+//                 backSpeed={100}
+//                 loop={true}
+//             />
+//         </div>
+//     </div>
+//   );
+// };
+
+// export default NavBar;
