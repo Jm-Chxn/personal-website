cda477c640e960fdfa57b6b8a34d42efa8008b49
jim <jimcnerf@gmail.com>
2025-02-01 20:17:43 -0800

    Fixed typing animation and display name
    got the finally fix display animation and fix the css component of displaying

--- src/components/Home/Intro.jsx
+++ src/components/Home/Intro.jsx
@@ -3,18 +3,22 @@ import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 function Intro() {
   const [text] = useTypewriter({
-    words: ['Hello, I am a software developer', 'Welcome to the website!'],
+    words: [' student',' software engineer', ' developer'],
     loop: true,
     typeSpeed: 100,
     deleteSpeed: 50,
   });
 
   return (
-    <div className="App">
-      <h1 style={{ color: 'white' }}>
-        {text}
+    <div className="app">
+      <div className="container">
+        Hey, I'm Jim
+      </div>
+      <div className="animated-typing">
+        I am a 
+        <span>{text}</span>
         <Cursor />
-      </h1>
+      </div>
     </div>
   );
 }
--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './Navbar.css';
 
-
 const NavBar = () => {
-
   return (
     <div className='NavBar'>
         <ul className="Navmenu">
