1463fce07a9ffe2238fd5328e04ef1cf701607f0
jim <jimcnerf@gmail.com>
2025-02-03 19:59:45 -0800

    Added photo section for about me
    fixed some css components and added a photo section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import './About.css';
+import profilePic from '../../assets/pp.jpg'; // Adjust the path as necessary
 
 const About = () => {
   useEffect(() => {
@@ -16,18 +17,25 @@ const About = () => {
     const hiddenElements = document.querySelectorAll('.hidden');
     hiddenElements.forEach((el) => observer.observe(el));
 
+    return () => {
+      hiddenElements.forEach((el) => observer.unobserve(el));
+    };
   }, []);
 
   return (
     <div className="about">
       <section className="hidden">
-        <h1>About Me<div className="line"></div> </h1>
-        <p>
-          Hello, my name is Jim. Currently, I am a student at Simon Fraser University pursuing a dual degree program 
-          in Computing Science and a Master in Finance. I am passionate about software development and always looking 
-          for new opportunities to learn and grow. Outside of software development, I enjoy watching and playing sports 
-          such as soccer, tennis, and skiing.
-        </p>
+        <h1>About Me</h1>
+        <div className="line"></div>
+        <div className="content">
+          <img src={profilePic} alt="Profile" className="profile-pic" />
+          <p>
+            Hello, my name is Jim. Currently, I am a student at Simon Fraser University pursuing a dual degree program 
+            in Computing Science and a Master in Finance. I am passionate about software development and always looking 
+            for new opportunities to learn and grow. Outside of software development, I enjoy watching and playing sports 
+            such as soccer, tennis, and skiing.
+          </p>
+        </div>
       </section>
     </div>
   );
