52a0011b4a78082d4cd45793fb436c6d5475b904
jim <jimcnerf@gmail.com>
2025-02-05 13:16:28 -0800

    We fixed sizing for screening and added second project
    We fixed when user adjusts the screen and we also made it so now i have two projects

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -8,12 +8,39 @@ const Project = () => {
       <div className="autoShow">
         <h1>Projects</h1>
       </div>
-        <img src={profilePic} alt="Profile" className="picture" />
+      <div className="content-container">
         <div className="projectline">
           <h2>01. Emergency Dashboard</h2>
           <p>A website that allows users to report emergencies which let responders know where the emergency is located and what kind of emergency it is.</p>
           <p style={{fontSize:"25px"}}>Technologies: React, JavaScript, TypeScript</p>
-          <a style={{fontSize:"25px", fontWeight: "50" }} href="https://cmpt272.vercel.app/"target="_blank" rel="noopener noreferrer" className="link">Live Website</a>
+          <a 
+            style={{fontSize:"25px", fontWeight: "50"}} 
+            href="https://cmpt272.vercel.app/" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="link"
+          >
+            Live Website
+          </a>
+        </div>
+        <img src={profilePic} alt="Profile" className="picture" />
+      </div>
+      <div className="content-container">
+        <div className="projectline">
+          <h2>02. Quizzify</h2>
+          <p>An interactive quiz platform that allows users to create, share, and participate in customized quizzes on various topics.</p>
+          <p style={{fontSize:"25px"}}>Technologies: React.js, JavaScript, Python, Django</p>
+          <a 
+            style={{fontSize:"25px", fontWeight: "50"}} 
+            href="https://github.com/AmarKoonar/Hackathon-quiz-app" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="link"
+          >
+            Github
+          </a>
+        </div>
+        <img src={profilePic} alt="Profile" className="picture" />
       </div>
     </div>
   );
